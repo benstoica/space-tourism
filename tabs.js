d87bebbd4a7956343770753aa10ab4525a40cb04
Ben Stoica <bstoica@uwm.com>
2021-11-18 13:25:02 -0500

    changing the image

--- tabs.js
+++ tabs.js
@@ -37,6 +37,7 @@ function changeTabFocus(e) {
 function changeTabPanel(e) {
   const targetTab = e.target;
   const targetPanel = targetTab.getAttribute("aria-controls");
+  const targetImage = targetTab.getAttribute("data-image");
 
   const tabContainer = targetTab.parentNode;
   const mainContainer = tabContainer.parentNode;
@@ -46,4 +47,10 @@ function changeTabPanel(e) {
   });
 
   mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden");
+
+  mainContainer.querySelectorAll("picture").forEach((picture) => {
+    picture.setAttribute("hidden", true);
+  });
+
+  mainContainer.querySelector([`#${targetImage}`]).removeAttribute("hidden");
 }
