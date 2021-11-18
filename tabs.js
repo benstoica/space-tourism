16978d86a1884fec42933487edf52a1edc098da6
Ben Stoica <bstoica@uwm.com>
2021-11-18 13:52:18 -0500

    refactoring

--- tabs.js
+++ tabs.js
@@ -14,24 +14,22 @@ function changeTabFocus(e) {
 
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
     tabs[tabFocus].setAttribute("tabindex", -1);
-  }
 
-  if (e.keyCode === keydownRight) {
-    tabFocus++;
-    if (tabFocus >= tabs.length) {
-      tabFocus = 0;
+    if (e.keyCode === keydownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+    } else if (e.keyCode === keydownLeft) {
+      tabFocus--;
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
     }
-  }
 
-  if (e.keyCode === keydownLeft) {
-    tabFocus--;
-    if (tabFocus < 0) {
-      tabFocus = tabs.length - 1;
-    }
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
   }
-
-  tabs[tabFocus].setAttribute("tabindex", 0);
-  tabs[tabFocus].focus();
 }
 
 function changeTabPanel(e) {
@@ -48,15 +46,19 @@ function changeTabPanel(e) {
 
   targetTab.setAttribute("aria-selected", true);
 
-  mainContainer.querySelectorAll("[role='tabpanel']").forEach((panel) => {
-    panel.setAttribute("hidden", true);
+  hideContent(mainContainer, "[role='tabpanel']");
+  showContent(mainContainer, [`#${targetPanel}`]);
+
+  hideContent(mainContainer, "picture");
+  showContent(mainContainer, [`#${targetImage}`]);
+}
+
+function hideContent(parent, content) {
+  parent.querySelectorAll(content).forEach((item) => {
+    item.setAttribute("hidden", true);
   });
+}
 
-  mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden");
-
-  mainContainer.querySelectorAll("picture").forEach((picture) => {
-    picture.setAttribute("hidden", true);
-  });
-
-  mainContainer.querySelector([`#${targetImage}`]).removeAttribute("hidden");
+function showContent(parent, content) {
+  parent.querySelector(content).removeAttribute("hidden");
 }
